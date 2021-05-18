3bef089964f81aa29902338e4fc0cbd9c366a371
mei-manzo <81221694+mei-manzo@users.noreply.github.com>
2021-05-17 21:51:00 -0700

    Update views.py
    debugged

--- mei_app/views.py
+++ mei_app/views.py
@@ -1,12 +1,22 @@
-# from django.shortcuts import render
+
 
 # Create your views here.
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+
 def index(request):
     return HttpResponse("placeholder to later display a list of all blogs")
 
-def index(new):
+def new(request):
     return HttpResponse("---placeholder to later display a list of all blogs")
 
-def index(create):
-    pass
+def create(request):
+    return redirect('/')
+
+def show(request, id):
+    return HttpResponse(f"placeholder to display blog number:{id}") #debug not showing actual number
+
+def edit(request, number):
+    return HttpResponse(f"placeholder to edit blog {number}")
+
+def destroy(request, number): 
+    return redirect('/')
